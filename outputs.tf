8f9513f04749b54646e46ad050f5fd67b3c42553
Kristo Aun <kristoaun@live.com>
2024-01-25 01:09:22 +0200

    lh

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "longhorn_dashboard" {
-  value = "kubectl -n longhorn-system port-forward service/longhorn-frontend 9999:80"
-}
-
 output "kubernetes_dashboard_token" {
   value = "kubectl -n kubernetes-dashboard create token kubernetes-dashboard"
 }
